fab4127a2944b0b1b78871a09f3d173893323a79
Jie JW Wu <122728498+jie-jw-wu@users.noreply.github.com>
2024-07-26 06:07:04 +0000

    sample fine-tuning for opt6.7b model

--- fine-tuning/fine_tuning.py
+++ fine-tuning/fine_tuning.py
@@ -17,19 +17,43 @@ from transformers import LlamaForSequenceClassification, LlamaTokenizer, Trainer
 from datasets import load_dataset
 from peft import LoraConfig, get_peft_model
 
+# fine-tuning tutorial: 
+## https://www.kaggle.com/code/lizhecheng/qlora-fine-tune-gpt-neox-20b
+## https://colab.research.google.com/drive/1jCkpikz0J2o20FBQmYmAGdiKmJGOMo-o?usp=sharing#scrollTo=cg3fiQOvmI3Q
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_name_or_path', type=str, help='Path to the model')
 parser.add_argument('--finetune_method', type=str, default='lora', help='fine-tuning method')
 parser.add_argument("-m","--model",help="LLM",type=str,required=True)
 parser.add_argument('--use_int8', action='store_true', help='whether to use int8 quantization')
 parser.add_argument('--use_fp16', action='store_true', help='whether to use fp16 precision')
+parser.add_argument("--dataset_path",help="dataset_path",type=str,required=True)
+parser.add_argument("--finetuned_model_path",help="finetuned_model_path",type=str,required=True)
+
+args = parser.parse_args()
 
 # TODO: Load the dataset
-dataset = load_dataset("code_x_glue_ct_code_to_text")
+# https://github.com/huggingface/datasets/issues/824#issuecomment-758358089
 
-# TODO: Assuming the dataset has a 'train' and 'validation' split
-train_dataset = dataset['train']
-validation_dataset = dataset['validation']
+#data = load_dataset("Abirate/english_quotes")
+
+data = datasets.load_from_disk(args.dataset_path)
+data = data.map(lambda samples: tokenizer(samples['quote']), batched=True)
 
 if args.use_int8:
     print("**********************************")
@@ -66,6 +90,7 @@ else:
         local_files_only=True,              
     )
 
+
 # configure tokenizer
 if (args.model.startswith('Meta-Llama')
     or args.model.startswith('deepseek')
@@ -87,16 +112,21 @@ else:
         offload_folder=offload_folder,
     )
 
-# Tokenize the dataset
-def tokenize_function(examples):
-    return tokenizer(examples['source'], padding="max_length", truncation=True)
 
-train_dataset = train_dataset.map(tokenize_function, batched=True)
-validation_dataset = validation_dataset.map(tokenize_function, batched=True)
+### Post-processing on the model
+# Finally, we need to apply some post-processing on the 8-bit model to enable training, let's freeze all our layers, and cast the layer-norm in `float32` for stability. We also cast the output of the last layer in `float32` for the same reasons.
+for param in model.parameters():
+  param.requires_grad = False  # freeze the model - train adapters later
+  if param.ndim == 1:
+    # cast the small parameters (e.g. layernorm) to fp32 for stability
+    param.data = param.data.to(torch.float32)
 
-# Set the format of the datasets to return PyTorch tensors
-train_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'label'])
-validation_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'label'])
+model.gradient_checkpointing_enable()  # reduce number of stored activations
+model.enable_input_require_grads()
+
+class CastOutputToFloat(nn.Sequential):
+  def forward(self, x): return super().forward(x).to(torch.float32)
+model.lm_head = CastOutputToFloat(model.lm_head)
 
 # Define the training arguments
 training_args = TrainingArguments(
@@ -122,14 +152,14 @@ if args.finetune_method == "lora":
     )
 
     # Apply LoRA to the model
-    model_lora = get_peft_model(model, lora_config)
+    model = get_peft_model(model, lora_config)
+    print_trainable_parameters(model)
 
     # Define the Trainer
     trainer = Trainer(
-        model=model_lora,
+        model=model,
         args=training_args,
         train_dataset=train_dataset,
-        eval_dataset=validation_dataset,
     )
 else:
     # Define the Trainer
@@ -137,12 +167,17 @@ else:
         model=model,
         args=training_args,
         train_dataset=train_dataset,
-        eval_dataset=validation_dataset,
     )
 
 # Train the model
 trainer.train()
 
-# Evaluate the model
-results = trainer.evaluate()
-print(results)
+model.save_pretrained(args.finetuned_model_path)
+
+# Inference
+batch = tokenizer("Two things are infinite: ", return_tensors='pt')
+
+with torch.cuda.amp.autocast():
+  output_tokens = model.generate(**batch, max_new_tokens=50)
+
+print('\n\n', tokenizer.decode(output_tokens[0], skip_special_tokens=True))
