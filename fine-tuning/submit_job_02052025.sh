f2654ca4de020db5068cb24797ddfa155f071c79
jie-jw-wu <jiewu@gwu.edu>
2025-02-23 18:41:22 -0500

    changes

--- fine-tuning/submit_job_02052025.sh
+++ fine-tuning/submit_job_02052025.sh
@@ -11,4 +11,4 @@ module load python rust cuda arrow/17.0.0
 source ~/ENV/bin/activate
 cd $SLURM_SUBMIT_DIR
 echo "we are in dir $SLURM_SUBMIT_DIR"
-python clarify_aware_fine_tuning_v2.py --dataset_path ../FINAL_finetuning_data_ques_only.jsonl --model_name_or_path /project/def-fard/jie/deepseek-ai/deepseek-coder-6.7b-instruct  --output_dir output-dir --tokenize_version 2 --finetuned_model_path /project/def-fard/jie/finetuned_models/deepseek-coder-6.7b-instruct-finetuned-02052025 > result_02052025.txt
+python clarify_aware_fine_tuning_v2.py --dataset_path /project/def-fard/jie/finetuning_data/FINAL_finetuning_data_ques_only.json --model_name_or_path /project/def-fard/jie/deepseek-ai/deepseek-coder-6.7b-instruct  --output_dir output-dir --tokenize_version 2 --finetuned_model_path /project/def-fard/jie/finetuned_models/deepseek-coder-6.7b-instruct-finetuned-02052025 > result_02052025.txt
